2775677a8628f24532eb55c501f081421a39fc41
Adrien Carpentier <moi@adriencarpentier.com>
2021-04-07 20:04:52 +0900

    Use english dictionnary for search strings, store in local storage for future requests in case YouTube API quota is depleted

--- index.js
+++ index.js
@@ -1,4 +1,79 @@
-function getRandomVideoSearch() {
+window.onload = () => {
+	fetch('config.json')
+		.then(response => response.json())
+		.then(config => {
+			getNewVideo(config.youtubeAPIkey)
+			document.getElementById("try-again").addEventListener("click", function () {
+				getNewVideo(config.youtubeAPIkey)
+			})
+		})
+}
+
+async function getNewVideo(ytAPIkey) {
+
+	let ytSearchString = await getRandomWord()
+
+	const url = new URL("https://www.googleapis.com/youtube/v3/search")
+	let params = {
+		key: ytAPIkey,
+		part: 'snippet',
+		type: 'video',
+		q: ytSearchString
+	}
+	url.search = new URLSearchParams(params).toString()
+	fetch(url)
+		.then((response) => {
+			if (response.ok) {
+				return response.json()
+			} else {
+				console.log(response)
+				launchPlayer(ytSearchString, null) // launch player without any YouTube ID, hopefully it will get one ID from the already requested ones
+				throw new Error("Bad YouTube response. Cannot parse.")
+			}
+		})
+		.then((responseJson) => {
+			if (responseJson.items.length) {
+				const items = responseJson.items
+				// take a video from the fresh response, not from the cache
+				const item = items[Math.floor(Math.random() * items.length)]
+				// we got a response so we will store all the ids in the localStorage list of YouTube Ids
+				// first, delete the video we just took from the list of videos to stores, so we won't watch it again
+				delete items.item
+				// retrieve the localStorage (or create a blank array if there isn't any info saved yet)
+				let youtubeIds = JSON.parse(localStorage.getItem('youtubeIds')) || []
+				items.forEach(item => {
+					youtubeIds.push(item.id.videoId)
+				})
+				// then put it back with ytSearchString as key
+				localStorage.setItem('youtubeIds', JSON.stringify(youtubeIds))
+				// finally, launch the player with the ID
+				launchPlayer(item.id.videoId)
+			} else {
+				console.log("Couldn't read YouTube search response.")
+				launchPlayer(null) // launch player without any YouTube ID, hopefully it will get one ID from the already requested ones
+			}
+		})
+		.catch((error) => {
+			console.log(error)
+		})
+}
+
+function launchPlayer(ytId) {
+	if (!ytId) {
+		let youtubeIds = JSON.parse(localStorage.getItem('youtubeIds'))
+		if (youtubeIds.length) {
+			console.log("Getting video ID from already requested list in local storage.")
+			ytId = youtubeIds[Math.floor(Math.random() * youtubeIds.length)]
+			delete youtubeIds.ytId // don't watch it a second time
+			localStorage.setItem('youtubeIds', JSON.stringify(youtubeIds))
+		} else {
+			throw new Error("Cannot get any YouTube ID.")
+		}
+	}
+	document.getElementById("yt").src = '//www.youtube.com/embed/' + ytId + '?rel=0'
+}
+
+function getRandomString() {
 	const codeLength = 5
 	const possibles = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890-_"
 	let code = "";
@@ -8,68 +83,10 @@ function getRandomVideoSearch() {
 	return code
 }
 
-window.onload = () => {
-
-	let ytAPIkey = ""
-	let maxAttempts = 5
-
-	fetch('config.json')
-		.then(response => response.json())
-		.then(data => {
-			ytAPIkey = data.youtubeAPIkey
-			maxAttempts = data.maxAttempts
-		})
-
-	var attempts = 0
-
-	function getNewVideo() {
-		attempts++
-
-		if (attempts <= maxAttempts) {
-			document.getElementById("try-again").disabled = true
-			let ytSearchString = getRandomVideoSearch()
-
-			const url = new URL("https://www.googleapis.com/youtube/v3/search")
-			let params = {
-				key: ytAPIkey,
-				part: 'snippet',
-				type: 'video',
-				q: ytSearchString
-			}
-
-			url.search = new URLSearchParams(params).toString()
-
-			fetch(url)
-				.then((response) => {
-					if (response.ok) {
-						return response.json()
-					} else {
-						throw new Error('Something went wrong')
-					}
-				})
-				.then((responseJson) => {
-					if (responseJson.items.length) {
-						const items = responseJson.items
-						const ytId = items[Math.floor(Math.random() * items.length)].id.videoId
-						document.getElementById("yt").src = '//www.youtube.com/embed/' + ytId + '?rel=0&autoplay=1'
-						document.getElementById("try-again").disabled = false
-					}
-					else {
-						getNewVideo()
-					}
-				})
-				.catch((error) => {
-					console.log(error)
-				})
-		} else {
-			document.getElementById("try-again").disabled = false
-		}
-	}
-
-	getNewVideo()
-
-	document.getElementById("try-again").addEventListener("click", function () {
-		getNewVideo()
-	})
-
+async function getRandomWord() {
+	const file = await fetch('dict_EN.txt')
+	const text = await file.text()
+	const lines = text.split("\n")
+	const randLineNum = Math.floor(Math.random() * lines.length)
+	return lines[randLineNum]
 }
